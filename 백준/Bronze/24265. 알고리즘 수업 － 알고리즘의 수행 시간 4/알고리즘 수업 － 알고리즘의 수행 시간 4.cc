7b2a0bee3ad5966e7544211d4505b09845be6b88
jini-coding <96573160+jini-coding@users.noreply.github.com>
2024-11-25 01:38:52 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,29 @@
+//
+//  24265.cpp
+//  Algorithm
+//
+//  Created by Jini on 11/25/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+//MenOfPassion(A[], n) { //배열 A와 n을 받음
+//    sum <- 0; //sum값을 0으로
+//    for i <- 1 to n - 1 //n-1번 수행
+//        for j <- i + 1 to n //n-i번 수행
+//           sum <- sum + A[i] × A[j]; # 코드1 //1번 수행, 총 n*(n-1)/2번 수행
+//    return sum;
+//} //항상 n*(n-1)/2번 수행, 시간 복잡도 O(n^2)이므로 최고차항의 차수는 2
+
+int main() {
+    long long n;
+    
+    cin >> n;
+    
+    cout << n * (n - 1) / 2 << "\n" << 2 << "\n";
+    
+    return 0;
+}
+
