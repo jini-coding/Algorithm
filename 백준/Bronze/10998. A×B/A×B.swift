3708d960a4aae080ee6ce47ce6e32c24e70f9e57
jini-coding <96573160+jini-coding@users.noreply.github.com>
2024-12-15 23:53:22 +0900

    [Bronze V] Title: A×B, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.swift
@@ -0,0 +1,20 @@
+//
+//  10998.swift
+//  Algorithm
+//
+//  Created by Jini on 12/15/24.
+//
+
+import Foundation
+
+let input = readLine()!
+
+let num = input.split(separator: " ").map { Int($0)! }
+
+let a = num[0]
+let b = num[1]
+
+print(a * b)
+
+
+
