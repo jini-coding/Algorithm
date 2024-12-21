6ec11a8eef744caab2806cee934d1afb8c62faa2
jini-coding <96573160+jini-coding@users.noreply.github.com>
2024-12-21 21:32:41 +0900

    [Bronze V] Title: 합, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.swift
@@ -0,0 +1,18 @@
+//
+//  8393.swift
+//  Algorithm
+//
+//  Created by Jini on 12/21/24.
+//
+
+import Foundation
+
+var sum = 0
+
+let n = Int(readLine()!)!
+
+for i in 1...n {
+    sum += i
+}
+
+print(sum)
