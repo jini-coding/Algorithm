7946c1b94d0e06e1b75a4f01f58381933fa808c3
jini-coding <96573160+jini-coding@users.noreply.github.com>
2024-12-29 23:58:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.swift
@@ -0,0 +1,14 @@
+//
+//  27866.swift
+//  Algorithm
+//
+//  Created by Jini on 12/29/24.
+//
+
+import Foundation
+
+let str = readLine()!.map { $0 }
+let idx = Int(readLine()!)!
+
+print(str[idx-1])
+
