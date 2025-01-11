dc56d358bce37ed473935372e2d12cf5f3b9cbe7
jini-coding <96573160+jini-coding@users.noreply.github.com>
2025-01-11 23:20:11 +0900

    [Bronze V] Title: 새싹, Time: 8 ms, Memory: 79500 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.swift
@@ -0,0 +1,17 @@
+//
+//  25083.swift
+//  Algorithm
+//
+//  Created by Jini on 1/11/25.
+//
+
+import Foundation
+
+print(#"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+"""#)
